a133a7da37dace78ed21e33b85eb1ad35a0c784b
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-07-23 19:35:42 +0200

    [add] readme file, [add] structure and [add] basic grid layout

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log("Prova");
+
+//Database
+const cards = [
+  {
+    name: "Prova gatto",
+    image:
+      "https://img.etimg.com/thumb/width-1600,height-900,imgsize-21060,resizemode-75,msid-100628729/news/international/us/exploding-kittens-on-netflix-see-cast-release-date-and-more.jpg",
+  },
+  {
+    name: "Prova gatto",
+    image:
+      "https://img.etimg.com/thumb/width-1600,height-900,imgsize-21060,resizemode-75,msid-100628729/news/international/us/exploding-kittens-on-netflix-see-cast-release-date-and-more.jpg",
+  },
+];
+
+//Dati
+const gridContainer = document.querySelector(".grid_container");
+
+//Funzioni
+//Popolamento griglia di gioco
