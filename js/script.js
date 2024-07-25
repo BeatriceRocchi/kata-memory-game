d35fb629dec094b831bb9e17946ca1b4770abfdd
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-07-25 18:03:02 +0200

    [add] logic of flipping card

--- js/script.js
+++ js/script.js
@@ -1,43 +1,67 @@
 //Database
 let cards = [
   {
-    name: "Airedale Terrier",
+    name: "San Francisco",
+    image: "SFO.webp",
   },
   {
-    name: "Corgie",
+    name: "Bryce Canyon",
+    image: "BryceCanyon.webp",
   },
   {
-    name: "Pastore Tedesco",
+    name: "Zion Park",
+    image: "ZionPark.webp",
   },
   {
-    name: "Golden Retriever",
+    name: "Grand Canyon",
+    image: "GrandCanyon.webp",
   },
   {
-    name: "Airedale Terrier",
+    name: "Las Vegas",
+    image: "LasVegas.webp",
   },
   {
-    name: "Corgie",
+    name: "Yosemite",
+    image: "Yosemite.webp",
   },
   {
-    name: "Pastore Tedesco",
+    name: "San Francisco",
+    image: "SFO.webp",
   },
   {
-    name: "Golden Retriever",
+    name: "Bryce Canyon",
+    image: "BryceCanyon.webp",
+  },
+  {
+    name: "Zion Park",
+    image: "ZionPark.webp",
+  },
+  {
+    name: "Grand Canyon",
+    image: "GrandCanyon.webp",
+  },
+  {
+    name: "Las Vegas",
+    image: "LasVegas.webp",
+  },
+  {
+    name: "Yosemite",
+    image: "Yosemite.webp",
   },
 ];
 
 //DATI
 const gridContainer = document.getElementById("grid-container");
 const btnStart = document.getElementById("btn-start");
-const cardsList = document.querySelectorAll(".card");
+const errorMsg = document.getElementById("error-msg");
 let counterFlip = 0;
 let flippedCards = [];
+let errors = 0;
 
 //SVOLGIMENTO
 // Al click sul bottone di start, l'array contenente le carte viene mescolato e le carte vengono inserite in griglia nell'ordine estratto
 btnStart.addEventListener("click", function () {
   cards = shuffleCards(cards);
-  console.log(cards);
 
   generateGrid();
 });
@@ -64,26 +88,35 @@ function generateGrid() {
 // Creazione delle card
 function createCard(i) {
   const card = document.createElement("div");
+  card.innerHTML = `<div class="card-inner">
+    <div class="card-front">
+      <img src="assets/img/USA.jpg" alt="Logo USA">
+    </div>
+    <div class="card-back">
+      <h1>${cards[i].name}</h1>
+      <img src="assets/img/${cards[i].image}" alt="${cards[i].name}"> 
+    </div>
+  </div>`;
   card.className = "card";
   card._cardID = i;
 
+  // In ascolto dell'evento di click sulla card: al click, viene richiamata la funzione per girare la carta
   card.addEventListener("click", function () {
-    console.log("cliccata", card._cardID);
     counterFlip++;
 
     flipCard(card._cardID);
   });
 
-  // Click e giro delle card
+  // Prima di procedere a girare la carta viene effettuato un controllo per verificare che sia la prima o seconda carta cliccata (per evitare utente clicchi più carte velocemente prima che venga effettuato il controllo di match sulla carte girate). Se il contatore delle carte girate è pari a 2, viene effettuato il controllo di match sulla carte
   function flipCard(i) {
     if (counterFlip < 3) {
-      card.innerHTML = cards[i].name;
+      let cardInner = document.querySelectorAll(".card-inner");
+      cardInner[i].style.webkitTransform = "rotateY(180deg)";
 
       flippedCards.push(card);
-      console.log(flippedCards);
 
       if (counterFlip === 2) {
-        setTimeout(checkCardsMatch, 2000);
+        setTimeout(checkCardsMatch, 1000);
       }
     }
   }
@@ -94,14 +127,15 @@ function createCard(i) {
 // Verifica match delle card girate
 function checkCardsMatch() {
   if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
-    console.log("Uguale");
     for (let flippedCard of flippedCards) {
       flippedCard.classList.add("block-click");
     }
   } else {
-    console.log("Diverso");
+    errors++;
     for (let flippedCard of flippedCards) {
-      flippedCard.innerHTML = "";
+      let cardInner = flippedCard.querySelector(".card-inner");
+      cardInner.style.webkitTransform = "rotateY(0)";
+      errorMsg.innerHTML = "Errori: " + errors;
     }
   }
 
